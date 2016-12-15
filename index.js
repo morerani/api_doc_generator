1cc14a433498e139dded98fc48068b2b6f05769c
Rani More <morerani2102@gmail.com>
2016-12-15 18:56:34 +0530

    added

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-var editor,editor2;
-$(function() {
+var editor, editor2;
+$(function () {
     editor = CodeMirror.fromTextArea(jsonarea, {
-                lineWrapping: false,
-                lineNumbers: true,
-                styleActiveLine: true,
-                mode: { name: "javascript", json: true },
-                  theme:'rubyblue'  
-            });
-editor.setSize(600, 400);
-              editor2 = CodeMirror.fromTextArea(result, {
-                lineWrapping: false,
-                lineNumbers: true,
-                styleActiveLine: true,
-                mode: { name: "javascript", json: true },
-                  theme:'rubyblue' 
-            });
-            editor2.setSize(600, 400);
-    $('#btnSubmit').click(function(e) {
+        lineWrapping: false,
+        lineNumbers: true,
+        styleActiveLine: true,
+        // mode: { name: "javascript", json: true },
+        mode: "application/json",
+        gutters: ["CodeMirror-lint-markers"],
+        lint: true,
+        theme: 'rubyblue'
+    });
+    editor.setSize(600, 400);
+    editor2 = CodeMirror.fromTextArea(result, {
+        lineWrapping: false,
+        lineNumbers: true,
+        styleActiveLine: true,
+        mode: { name: "javascript", json: true },
+        theme: 'rubyblue'
+    });
+    editor2.setSize(600, 400);
+    $('#btnSubmit').click(function (e) {
         e.preventDefault();
-       // var json = $('#jsonarea').val();
-       var json=editor.getValue();
-   
+        // var json = $('#jsonarea').val();
+        var json = editor.getValue();
+
         var objJson = $.parseJSON(json);
         console.log(objJson);
-        var param="\n* @apiParam {";
-        var successParam="\n* @apiSuccess {";
+        var param = "\n* @apiParam {";
+        var successParam = "\n* @apiSuccess {";
 
-       writeApi(objJson,param);
+        writeApi(objJson, param);
         editor2.replaceRange("\n", CodeMirror.Pos(editor.lastLine()));
-       writeApi(objJson,successParam);
+        writeApi(objJson, successParam);
 
     });
 
@@ -36,26 +39,26 @@ editor.setSize(600, 400);
 });
 
 
-function writeApi(objJson,params){
-   
-     for (var key in objJson) {
-            if (objJson.hasOwnProperty(key)) {
-                var element = objJson[key];
-debugger
-                if (typeof element == "object") {
-                    ParseObject(element, key,params);
-                    continue;
-                }
-              
-                var str = params+ (typeof element).toString() + "}" + key + " " + titleCase(key) + " of the OBJECT";
-                Log(str)
+function writeApi(objJson, params) {
+
+    for (var key in objJson) {
+        if (objJson.hasOwnProperty(key)) {
+            var element = objJson[key];
+            debugger
+            if (typeof element == "object") {
+                ParseObject(element, key, params);
+                continue;
             }
 
+            var str = params + (typeof element).toString() + "}" + key + " " + titleCase(key) + " of the OBJECT";
+            Log(str)
         }
+
+    }
 }
 
 
-function ParseObject(objJson, objectName,paramName) {
+function ParseObject(objJson, objectName, paramName) {
 
     for (var key in objJson) {
         if (objJson.hasOwnProperty(key)) {
@@ -63,10 +66,10 @@ function ParseObject(objJson, objectName,paramName) {
 
             if (typeof element == "object") {
                 key = objectName + "." + key;
-                ParseObject(element, key,paramName);
+                ParseObject(element, key, paramName);
                 continue;
             }
-            var str = paramName + (typeof element).toString() + "}" + key + " " + titleCase(key) + " of the " + objectName ;
+            var str = paramName + (typeof element).toString() + "}" + key + " " + titleCase(key) + " of the " + objectName;
             Log(str)
         }
 
@@ -89,5 +92,5 @@ function titleCase(str) {
 
 function Log(msg) {
     //editor2.append(msg);
-     editor2.replaceRange(msg, CodeMirror.Pos(editor.lastLine()));
+    editor2.replaceRange(msg, CodeMirror.Pos(editor.lastLine()));
 }
